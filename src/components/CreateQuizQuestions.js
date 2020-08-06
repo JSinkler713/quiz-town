f36436b82351cf3305c4a756105d3f168d32cfdd
James Sinkler <jamessinkler713@gmail.com>
2020-08-05 21:40:01 -0700

    adds create functionality for questions associated with a certain quiz

--- src/components/CreateQuizQuestions.js
+++ src/components/CreateQuizQuestions.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import QuizModel from '../models/quiz';
+import QuestionModel from '../models/question';
 import Modal from 'react-modal';
 import {
   BrowserRouter as Router,
@@ -12,10 +13,22 @@ import '../main.scss'
 
 function CreateQuizQuestions(props) {
   const [question, setQuestion] = useState('')
-  const [answer1, setAnswer1] = useState('')
-  const [answer2, setAnswer2] = useState('')
-  const [answer3, setAnswer3] = useState('')
-  const [answer4, setAnswer4] = useState('')
+  const [answer1, setAnswer1] = useState({
+        text: '',
+        correct: false
+  })
+  const [answer2, setAnswer2] = useState({
+         text: '',
+         correct: false
+  })
+  const [answer3, setAnswer3] = useState({
+         text: '',
+         correct: false
+  })
+  const [answer4, setAnswer4] = useState({
+         text: '',
+         correct: false
+  })
   const [realQuiz, setRealQuiz] = useState()
   const [show, setShow] = useState(false)
 
@@ -28,21 +41,138 @@ function CreateQuizQuestions(props) {
     if (e.target.id === 'question') {
       setQuestion(e.target.value);
     } else if (e.target.id === 'answer1') {
-      setAnswer1(e.target.value);
+      let holder = {
+        correct: answer1.correct,
+        text: e.target.value
+      }
+      setAnswer1(holder);
     } else if (e.target.id === 'answer2') {
-      setAnswer2(e.target.value);
+      let holder = {
+        correct: answer2.correct,
+        text: e.target.value
+      }
+      setAnswer2(holder);
     } else if (e.target.id === 'answer3') {
-      setAnswer3(e.target.value);
+      let holder = {
+        correct: answer3.correct,
+        text: e.target.value
+      }
+      setAnswer3(holder);
     } else if (e.target.id === 'answer4') {
-      setAnswer4(e.target.value);
-    }
+      let holder = {
+        correct: answer3.correct,
+        text: e.target.value
+      }
+      setAnswer4(holder);
+    } 
+
+    // these are click events
+    // for the check marks
   }
+
+  const handleTrueClick = (e) => {
+    if (e.target.id === 'answer1Correct') {
+      let holder = {
+        text: answer1.text,
+        correct: true
+      }
+      setAnswer1(holder);
+    } else if (e.target.id === 'answer2Correct') {
+      let holder = {
+        text: answer2.text,
+        correct: true
+      }
+      setAnswer2(holder);
+    } else if (e.target.id === 'answer3Correct') {
+      let holder = {
+        text: answer3.text,
+        correct: true
+      }
+      setAnswer3(holder);
+    } else if (e.target.id === 'answer4Correct') {
+      let holder = {
+        text: answer4.text,
+        correct: true
+      }
+      setAnswer4(holder);
+    }
+  } 
+  const handleFalseClick = (e) => {
+    if (e.target.id === 'answer1Wrong') {
+      let holder = {
+        text: answer1.text,
+        correct: false
+      }
+      setAnswer1(holder);
+    } else if (e.target.id === 'answer2Wrong') {
+      let holder = {
+        text: answer2.text,
+        correct: false
+      }
+      setAnswer2(holder);
+    } else if (e.target.id === 'answer3Wrong') {
+      let holder = {
+        text: answer3.text,
+        correct: false
+      }
+      setAnswer3(holder);
+    } else if (e.target.id === 'answer4Wrong') {
+      let holder = {
+        text: answer4.text,
+        correct: false
+      }
+      setAnswer4(holder);
+    }
+  } 
+
+
   async function createQuestion(e) {
     e.preventDefault()
-    console.log('click submit with value', question)
-    //const Quiz = await Promise.resolve(QuizModel.create(quizName))
-    //await console.log(Quiz)
-    //await setRealQuiz(Quiz)
+    //check if there is 1 correct response
+    let answers = [ answer1, answer2, answer3, answer4]
+    let count = 0; 
+    answers.forEach(answer=> {
+      if (answer.correct) {
+        count++
+      }
+    })
+    //structure the questionObject for the fetch request
+    let questionObject = {
+      prompt: question,
+      answers,
+      quiz: realQuiz.quiz._id 
+    }
+    console.log(questionObject)
+    if (count === 1) {
+      console.log('it is ready to be uploaded')
+      setShow(true)
+
+    } else {
+      console.log('choose One true answer')
+    }
+    const Question = await Promise.resolve(QuestionModel.create(questionObject))
+    await console.log(Question)
+
+  }
+        
+  const resetFields = ()=> {
+    //reset fields
+
+    setShow(false)
+    let answersArray = document.querySelectorAll('.answer')
+    answersArray.forEach((el)=> {
+      el.placeholder = ''
+      el.value = ''
+    });
+    //reset state of input
+    let holder = {
+      text: '',
+      correct: false
+    }
+    setAnswer1(holder);
+    setAnswer2(holder);
+    setAnswer3(holder);
+    setAnswer4(holder);
   }
 
   Modal.setAppElement(document.getElementById('root'));
@@ -56,6 +186,7 @@ function CreateQuizQuestions(props) {
           <img src='/hammer.png' />
         </div>
 
+        <form>
         <div className='question'>
           <h2>Q.</h2>
           <textarea id='question' rows='4' cols='40' type='text' onChange={handleChange} placeholder='Type your Question here' />
@@ -66,41 +197,42 @@ function CreateQuizQuestions(props) {
             <h3> A:</h3>
             <input id='answer1' className='answer' onChange={handleChange} placeholder='write your answer here'></input>
             <div className='wrong-right'>
-                <img src='/checkmark.png' />
-                <img src='/cross.png' />
+                <img id='answer1Correct' src='/checkmark.png' onClick={handleTrueClick} />
+                <img id='answer1Wrong' src='/cross.png' onClick={handleFalseClick} />
             </div>
           </div>
           <div className='answer'> 
             <h3> A:</h3>
             <input id='answer2' className='answer' onChange={handleChange} placeholder='write your answer here'></input>
             <div className='wrong-right'>
-                <img src='/checkmark.png' />
-                <img src='/cross.png' />
+                <img id='answer2Correct' src='/checkmark.png' onClick={handleTrueClick} />
+                <img id='answer2Wrong' src='/cross.png' onClick={handleFalseClick} />
             </div>
           </div>
           <div className='answer'> 
             <h3> A:</h3>
             <input  id='answer3' className='answer' onChange={handleChange} placeholder='write your answer here'></input>
             <div className='wrong-right'>
-                <img src='/checkmark.png' />
-                <img src='/cross.png' />
+                <img id='answer3Correct' src='/checkmark.png' onClick={handleTrueClick} />
+                <img id='answer3Wrong' src='/cross.png' onClick={handleFalseClick} />
             </div>
           </div>
           <div className='answer'> 
             <h3> A:</h3>
             <input  id='answer4' className='answer' onChange={handleChange} placeholder='write your answer here'></input>
             <div className='wrong-right'>
-                <img src='/checkmark.png' />
-                <img src='/cross.png' />
+                <img id='answer4Correct' src='/checkmark.png' onClick={handleTrueClick} />
+                <img id='answer4Wrong' src='/cross.png' onClick={handleFalseClick} />
             </div>
           </div>
         </div>
+          <button onClick={createQuestion} />
+        </form>
         <Modal isOpen={show} >
-          <h2>That question is taken</h2>
+          <h2>Adding your question</h2>
           <h3></h3>
-          <Link to='/'>
-            <h2>Add Questions</h2>
-          </Link>
+          <button onClick={resetFields}><h2>Add more Questions?</h2></button>
+          <a href='#'> Placeholder, finish quiz</a>
         </Modal>
       </div>
     );
