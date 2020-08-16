0a83ea144a3a6d289eac43c7e2dfcb99f833cba1
James Sinkler <jamessinkler713@gmail.com>
2020-08-15 17:05:11 -0700

    adds state to Quiz component, tracks correct answers, displays result at end of quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,18 +1,57 @@
 import React, {useState, useEffect} from 'react';
-
+import {Link} from "react-router-dom"
+import Question from './Question'
 const Quiz = (props)=> {
     const [quiz, setQuiz] = useState(props.location.state.quiz)
-    const [question, setQuestion] = useState(props.location.state.quiz.questions[0].prompt)
-    const [answers, setAnswers] = useState(props.location.state.quiz.questions[0].answers)
+    const [gameOver, setGameOver] = useState(false)
+    //hold questionNumber in state
+    //as each question is answered, update question Number
+    const [questionNumber, setQuestionNumber] = useState(0)
+    //which in turn updates question
+    const [question, setQuestion] = useState(props.location.state.quiz.questions[questionNumber].prompt)
+    //which in turn updates answers
+    const [answers, setAnswers] = useState(props.location.state.quiz.questions[questionNumber].answers)
+    //which in turn updates correctAnswer
     const [correctAnswer, setCorrectAnswer] = useState()
+    //keep track of totalCorrect
+    const [totalCorrect, setTotalCorrect] = useState(0)
+    
+    //checking if I have quiz
+    //I do have it as props.location.state.quiz
     useEffect(()=> {
-        console.log(props.location.state.quiz)
-      }, [])
+       setQuestion(quiz.questions[questionNumber].prompt) 
+       setAnswers(quiz.questions[questionNumber].answers)
+      }, [questionNumber])
 
+    const checkTrue = (truth)=> {
+        console.log(truth)
+        if (truth) {
+            setTotalCorrect((prevState)=> prevState+1)
+        }
+        if (questionNumber === props.location.state.quiz.questions.length - 1) {
+            console.log('game over yo')
+            setGameOver(true)
+            //Link to display page, or show component display page
+        } else {
+            setQuestionNumber((prevState)=> prevState + 1)
+        }
+    }
     return (
         <div>
-           {props.match.params.id}
-        { quiz.name ? quiz.name : 'hmmm'}
+        <h1>{ quiz.name ? quiz.name : 'hmmm'}</h1>
+        <div className='question-holder'>
+        { !gameOver ? <h2>{question}</h2> : <h2>Nice Work</h2> }
+            { !gameOver ? answers.map((answer,i)=> {
+                return (
+                    <div>
+                    <Question answer={answer.text} checkTrue={checkTrue} correct={answer.correct} />
+                    </div>
+                )
+            })
+            : `You got ${totalCorrect}/${questionNumber + 1} correct.`
+        }
+        </div>
+            <Link to='/'>Back to Main</Link>
         </div>
     );
 }
